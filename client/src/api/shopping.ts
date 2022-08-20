775033fac96ea0be4a6787cbeb7855ad30ea9d9b
bora <a52447879@gmail.com>
2022-08-20 20:57:37 +0900

    [remove] test axios 삭제

--- client/src/api/shopping.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const sendRequest = async (data: any) => {
-  const response = await axios.post('http://localhost:8080/shopping', data);
-  console.log(response);
-  console.log(response.data);
-};
