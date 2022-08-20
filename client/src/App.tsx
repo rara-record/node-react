f85bb4097c4cad2e3c831e5362157360fd814aa2
bora <a52447879@gmail.com>
2022-08-20 20:58:01 +0900

    [feat] chart js 테스트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import { sendRequest } from './api/shopping';
-import { UserInput } from './components';
+import React from 'react';
+
+import { Chart, UserInput } from './components';
 
 const App = () => {
-  return <UserInput />;
+  return (
+    <>
+      <UserInput />
+      <Chart />
+    </>
+  );
 };
 
 export default App;
